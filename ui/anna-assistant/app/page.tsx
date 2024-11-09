ad71294c9f1de92eeb044d00cb726bcbe1b03d19
Jupiter Bakakeu <jupiter.bakakeu@gmail.com>
2024-11-09 22:00:15 +0100

    Remove meet

--- ui/anna-assistant/app/page.tsx
+++ ui/anna-assistant/app/page.tsx
@@ -62,26 +62,10 @@ export default function Page() {
         </LiveKitRoom>
       </main>
       <footer className="hidden md:flex md:items-center md:gap-2 md:justify-end font-mono uppercase text-right pt-1 pb-2 px-8 text-xs text-gray-600 w-full md:mx-auto">
-        Built with
-        on
-        <a
-          href="https://github.com/livekit/agents"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="underline"
-        >
-          LiveKit Agents
-        </a>{" "}
-        •
-        <a
-          href="https://github.com/livekit-examples/realtime-playground"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="underline inline-flex items-center gap-1"
-        >
-          View source on GitHub
-        </a>
-        • © 2024 LiveKit
+        Built by Jupiter Bakakeu
+        {" "}
+
+        © 2024 Alteryx
       </footer>
     </div>
   );
